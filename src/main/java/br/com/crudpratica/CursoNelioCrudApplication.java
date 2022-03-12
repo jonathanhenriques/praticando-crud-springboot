3aba28653626fbf417892bd511267b30f5b8ee87
Jonathan Henrique <silva.henriquejonas@gmail.com>
2022-03-12 15:41:21 -0300

    Projeto criado

---
+++ src/main/java/br/com/crudpratica/CursoNelioCrudApplication.java
@@ -0,0 +1,13 @@
+package br.com.crudpratica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoNelioCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoNelioCrudApplication.class, args);
+	}
+
+}
