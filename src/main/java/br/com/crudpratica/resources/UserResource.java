fd56bffc6cb20f856e63df5a3ab44e649c341069
Jonathan Henrique <silva.henriquejonas@gmail.com>
2022-03-12 19:42:06 -0300

    Atualizamos o Service, anotamos os components

--- src/main/java/br/com/crudpratica/resources/UserResource.java
+++ src/main/java/br/com/crudpratica/resources/UserResource.java
@@ -1,21 +1,34 @@
 package br.com.crudpratica.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.crudpratica.entities.User;
+import br.com.crudpratica.services.UserService;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<User> finAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com","999999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> finAll(){
+		List<User> lista = service.findAll();
+		return ResponseEntity.ok().body(lista);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User usuario = service.findById(id);
+		return ResponseEntity.ok().body(usuario);
 	}
 	
 	
---
+++ src/main/java/br/com/crudpratica/services/UserService.java
@@ -0,0 +1,28 @@
+package br.com.crudpratica.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.crudpratica.entities.User;
+import br.com.crudpratica.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+		
+	}
+
+}
